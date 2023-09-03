fixing gradient sync and error in backprop after embeds

train.py
@@ -7,6 +7,8 @@ import sys
 import json
 from dataclasses import dataclass, field
 from typing import Optional
+import hashlib
+import functools
 
 import datasets
 import nltk  # Here to have a nice missing dependency error message early on
@@ -32,15 +34,21 @@ from torch.utils.data import Dataset, SequentialSampler
 from torch.utils.data.distributed import DistributedSampler
 from transformers.utils.versions import require_version
 from datasets import Dataset,DatasetDict
+import torch
 from torch import nn
 
 import torch.distributed as dist
 from transformers import utils
+from transformers import trainer_utils
 from typing import Dict, Union, Any
+
 if utils.is_sagemaker_mp_enabled():
     from transformers.trainer_pt_utils import smp_forward_backward
+
 if utils.is_apex_available():
     from apex import amp
+
+    
 
 check_min_version("4.20.0.dev0")
 
@@ -59,6 +67,27 @@ except (LookupError, OSError):
         nltk.download("punkt", quiet=True)
 
 MULTILINGUAL_TOKENIZERS = [MBartTokenizer, MBartTokenizerFast, MBart50Tokenizer, MBart50TokenizerFast]
+
+def tensor_info(tensor):
+    """Compute hash and statistics for a PyTorch tensor."""
+    # Compute hash
+    tensor_bytes = tensor.detach().cpu().numpy().tobytes()
+    hash_val = hashlib.sha256(tensor_bytes).hexdigest()
+    
+    # Compute statistics
+    mean_val = torch.mean(tensor).item()
+    max_val = torch.max(tensor).item()
+    min_val = torch.min(tensor).item()
+    std_val = torch.std(tensor).item()
+    
+    return {
+        "hash": hash_val,
+        "mean": mean_val,
+        "max": max_val,
+        "min": min_val,
+        "std": std_val
+    }
+
 
 def has_length(dataset):
     """
@@ -115,7 +144,7 @@ class InstructorTrainer(Seq2SeqTrainer):
         embeddings_query, embeddings_pos, embeddings_neg, cl_temperature = \
             cur_results['query'], cur_results['pos'], cur_results['neg'], self.args.cl_temperature
 
-        is_distributed = (self.args.local_rank != -1)
+        is_distributed = (self.args.process_index != -1)
         if is_distributed:
             gather_device_rank = 0 
             gathered_embeddings_query = [torch.zeros_like(embeddings_query) for _ in range(self.args.world_size)]
@@ -146,9 +175,12 @@ class InstructorTrainer(Seq2SeqTrainer):
             return contrastive_loss
 
         if not is_distributed:
-            loss_tensor = compute_constrastive_loss(embeddings_query, embeddings_pos, embeddings_neg, cl_temperature)
+            # <algo 0> 
+            loss_tensor = compute_constrastive_loss(embeddings_query, embeddings_pos, embeddings_neg, cl_temperature) # simple DDP
+            # </algo 0> 
         else:
             if self.args.process_index == gather_device_rank:
+                # <algo 1> contacanate the embeddings from gpus and then compute loss and send them back to respective gpus
                 gathered_embeddings_query = torch.cat(gathered_embeddings_query, dim=0).to(gather_device_rank)
                 gathered_embeddings_query.requires_grad_()
                 gathered_embeddings_pos = torch.cat(gathered_embeddings_pos, dim=0).to(gather_device_rank)
@@ -164,33 +196,51 @@ class InstructorTrainer(Seq2SeqTrainer):
                     self.printed_tensor_size = True
 
                 loss = compute_constrastive_loss(gathered_embeddings_query, gathered_embeddings_pos, gathered_embeddings_neg, cl_temperature)
-                loss_tensor = torch.tensor([loss.item()]).to(self.args.process_index)
+                scattered_loss_tensor = [torch.tensor([loss.item(),]).to(self.args.process_index) for _ in range(self.args.world_size)]
+                
                 loss.backward()
 
                 scattered_embeddings_query_grad = list(gathered_embeddings_query.grad.split(embeddings_query.size(0)))
                 scattered_embeddings_pos_grad = list(gathered_embeddings_pos.grad.split(embeddings_pos.size(0)))
                 scattered_embeddings_neg_grad = list(gathered_embeddings_neg.grad.split(embeddings_neg.size(0)))
+                # </algo 1> 
+
+                # # <algo 2> compute loss for embeddings from each gpu separately and send them back to respective gpus
+                # scattered_loss_tensor = []
+                # scattered_embeddings_query_grad, scattered_embeddings_pos_grad, scattered_embeddings_neg_grad = [], [], []
+                # for process_index, (cur_embeddings_query, cur_embeddings_pos, cur_embeddings_neg) in enumerate(zip(gathered_embeddings_query, gathered_embeddings_pos, gathered_embeddings_neg)):                    
+                #     cur_embeddings_query.requires_grad_()
+                #     cur_embeddings_pos.requires_grad_()
+                #     cur_embeddings_neg.requires_grad_()
+
+                #     cur_loss = compute_constrastive_loss(cur_embeddings_query, cur_embeddings_pos, cur_embeddings_neg, cl_temperature)
+                #     scattered_loss_tensor.append(torch.tensor([cur_loss.item(),]).to(self.args.process_index))
+
+                #     cur_loss.backward()
+
+                #     scattered_embeddings_query_grad.append(cur_embeddings_query.grad)
+                #     scattered_embeddings_pos_grad.append(cur_embeddings_pos.grad)
+                #     scattered_embeddings_neg_grad.append(cur_embeddings_neg.grad)
+                # # </algo 2> 
             else:
                 scattered_embeddings_query_grad, scattered_embeddings_pos_grad, scattered_embeddings_neg_grad = None, None, None
-                loss_tensor = torch.zeros(1).to(self.args.process_index)
+                scattered_loss_tensor = None
 
             embeddings_query_grad = torch.ones_like(embeddings_query)
             dist.scatter(embeddings_query_grad, scattered_embeddings_query_grad, src=gather_device_rank)
-            embeddings_query.grad = embeddings_query_grad 
 
             embeddings_pos_grad = torch.ones_like(embeddings_pos)
             dist.scatter(embeddings_pos_grad, scattered_embeddings_pos_grad, src=gather_device_rank)
-            embeddings_pos.grad = embeddings_pos_grad 
 
             embeddings_neg_grad = torch.ones_like(embeddings_neg)
             dist.scatter(embeddings_neg_grad, scattered_embeddings_neg_grad, src=gather_device_rank)
-            embeddings_neg.grad = embeddings_neg_grad 
 
-            dist.broadcast(loss_tensor, src=gather_device_rank)
+            loss_tensor = torch.tensor([1.,]).to(self.args.process_index)
+            dist.scatter(loss_tensor, scattered_loss_tensor, src=gather_device_rank)
 
-            embeddings_query.backward(torch.ones_like(embeddings_query)) #, retain_graph=True) 
-            embeddings_pos.backward(torch.ones_like(embeddings_pos)) #, retain_graph=True) 
-            embeddings_neg.backward(torch.ones_like(embeddings_neg)) 
+            embeddings_query.backward(embeddings_query_grad) 
+            embeddings_pos.backward(embeddings_pos_grad) 
+            embeddings_neg.backward(embeddings_neg_grad) 
 
         return loss_tensor, is_distributed
 
@@ -212,37 +262,47 @@ class InstructorTrainer(Seq2SeqTrainer):
         Return:
             `torch.Tensor`: The tensor with training loss on this batch.
         """
-        model.train()
-        inputs = self._prepare_inputs(inputs)
+        with model.no_sync():
+            model.train()
+            inputs = self._prepare_inputs(inputs)
 
-        if utils.is_sagemaker_mp_enabled():
-            loss_mb = smp_forward_backward(model, inputs, self.args.gradient_accumulation_steps)
-            return loss_mb.reduce_mean().detach().to(self.args.device)
+            if utils.is_sagemaker_mp_enabled():
+                loss_mb = smp_forward_backward(model, inputs, self.args.gradient_accumulation_steps)
+                return loss_mb.reduce_mean().detach().to(self.args.device)
 
-        with self.compute_loss_context_manager():
-            loss, is_distributed = self.compute_loss(model, inputs)
+            with self.compute_loss_context_manager():
+                loss, is_distributed = self.compute_loss(model, inputs)
 
-        if not is_distributed:
-            if self.args.n_gpu > 1:
-                loss = loss.mean()  # mean() to average on multi-gpu parallel training
+            if not is_distributed:
+                if self.args.n_gpu > 1:
+                    loss = loss.mean()  # mean() to average on multi-gpu parallel training
 
-            if self.args.gradient_accumulation_steps > 1 and not self.deepspeed:
-                # deepspeed handles loss scaling by gradient_accumulation_steps in its `backward`
-                loss = loss / self.args.gradient_accumulation_steps
+                if self.args.gradient_accumulation_steps > 1 and not self.deepspeed:
+                    # deepspeed handles loss scaling by gradient_accumulation_steps in its `backward`
+                    loss = loss / self.args.gradient_accumulation_steps
 
-            if self.do_grad_scaling:
-                self.scaler.scale(loss).backward()
-            elif self.use_apex:
-                with amp.scale_loss(loss, self.optimizer) as scaled_loss:
-                    scaled_loss.backward()
-            elif self.deepspeed:
-                # loss gets scaled under gradient_accumulation_steps in deepspeed
-                loss = self.deepspeed.backward(loss)
+                if self.do_grad_scaling:
+                    self.scaler.scale(loss).backward()
+                elif self.use_apex:
+                    with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                        scaled_loss.backward()
+                elif self.deepspeed:
+                    # loss gets scaled under gradient_accumulation_steps in deepspeed
+                    loss = self.deepspeed.backward(loss)
+                else:
+                    loss.backward()
             else:
-                loss.backward()
-        else:
-            # grads computed inside the compute_loss function itself
-            loss = loss[0]
+                # grads computed inside the compute_loss function itself
+                loss = loss[0]
+
+        # # print some gradients for debugging across various algos
+        # for rank in range(self.args.world_size):
+        #     if self.args.process_index == rank:
+        #         print(f"****** Rank {self.args.process_index}'s loss:", float(loss))
+        #         for name, param in list(model.named_parameters())[-5:]:
+        #             if param.grad is not None:
+        #                 print(f"Rank {self.args.process_index}, Parameter {name}'s gradient hash: {tensor_info(param.grad)}") 
+        #     dist.barrier()
 
         return loss.detach()
 